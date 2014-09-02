9db5d618ce70dede4ff57f15ce8ba7fb1a727c01
David J Maison Jr. <davem@interlinkone.com>
2014-09-02 11:02:53 -0400

    Ease of use
    - prevents break when expected objects don't exist
    - valid example json

--- js/vidya.js
+++ js/vidya.js
@@ -1,4 +1,8 @@
 function Vidya( obj ) {
+
+	if( typeof obj.controls != 'object') obj.controls = {};
+	if( typeof obj.movement != 'object') obj.movement = {};
+
 	this.collision = new Collision( obj );
 	this.collision.init();
 	this.collision.controls.init();
